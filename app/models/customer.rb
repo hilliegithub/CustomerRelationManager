9d5a4201fa1760961e65288deb6ebf2087eb9518
Hylton <goregehylton@yahoo.com>
2023-06-08 18:53:54 -0500

    Added records and validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
+    validates :fullname, :phone_number, presence: true
+    validates :fullname, length: {minimum: 3,
+    too_short: "%{count} characters is the minimum allowed"}
+    validates :email, uniqueness: true
     has_one_attached :image
 end
