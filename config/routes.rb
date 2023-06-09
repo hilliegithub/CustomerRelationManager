cda95343579f52d2efa2788ff06e5cb99e98374b
Hylton <goregehylton@yahoo.com>
2023-06-08 20:29:49 -0500

    Added routes and links

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "customers#index"
+
+  resources :customers do
+    collection do
+      get 'alphabetized'
+      get 'missing_email'
+    end
+  end
 end
+
