29ca30abd8692fcfd25f04a5e9cc7739bed3ee40
Hylton <goregehylton@yahoo.com>
2023-06-08 19:49:41 -0500

    Alphabetized action/view added

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,8 @@ class CustomersController < ApplicationController
     def index
         @customers = Customer.all
     end
+
+    def alphabetized
+        @customers = Customer.order(:fullname)
+    end
 end
